a8efff1507aad91635e98cac611c7361181a1ea8
Jos33y <josephlagbalu@gmail.com>
2020-05-10 04:19:55 +0100

    second commit

---
+++ css/header.php
@@ -0,0 +1,42 @@
+<header>
+    <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light sticky-top">
+      <button class="navbar-toggler " type="button" data-toggle="collapse" 
+      data-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon my-toggler "></span>
+        </button>
+        <div class="collapse navbar-collapse"></div>
+
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Open Government</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Elections</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Volunteer</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Donate</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Employment</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+  </header>
