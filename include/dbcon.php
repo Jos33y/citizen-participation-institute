b320350f6a3ff10a2819b5381b17b951c0667549
Jos33y <josephlagbalu@gmail.com>
2020-12-21 22:23:28 +0100

    updated database

--- include/dbcon.php
+++ include/dbcon.php
@@ -1,8 +1,8 @@
 <?php
 
 //$con = mysqli_connect("localhost", "root", "", "citizenparticipation");
-$con = mysqli_connect("127.0.0.1:49841", "azure", "6#vWHD_$", "citizenparticipation");
-
+$con = mysqli_connect("127.0.0.1:49905", "azure", "6#vWHD_$", "citizenparticipation");
+//$con = mysqli_connect("localhost", "root", "", "citizenparticipation");
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
    }
