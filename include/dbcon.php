7c657c724d053ad41885ffba5c5f916f47cb115a
Jos33y <josephlagbalu@gmail.com>
2021-03-24 18:23:43 -0700

    updated dbcon

--- include/dbcon.php
+++ include/dbcon.php
@@ -25,13 +25,22 @@ try {
 if (!$con) {
 die("Connection failed: " . mysqli_connect_error());
 }
-
+/*
 define('DB_DRIVER', 'mysql');
 define('DB_PREFIX', '');
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASSWORD", "");
+define("DB_DATABASE", "citizenparticipation"); */
+
+
+define('DB_DRIVER', 'mysql');
+define('DB_PREFIX', '');
+define("DB_HOST", "127.0.0.1:49905");
+define("DB_USER", "azure");
+define("DB_PASSWORD", "6#vWHD_$");
 define("DB_DATABASE", "citizenparticipation");
+
 
 try {
 $DB = new PDO(DB_DRIVER . ':host=' . DB_HOST . ';dbname=' . DB_DATABASE, DB_USER, DB_PASSWORD);
